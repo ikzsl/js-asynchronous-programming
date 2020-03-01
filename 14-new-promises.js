a541722e6415a4e789582b56810273f182e80388
ikzsl <ikzsl@yandex.ru>
2020-03-01 23:03:51 +0300

    add new promises

---
+++ 14-new-promises.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+// const promise = new Promise((resolve, reject) => {
+//   fs.readFile('file1', 'utf-8',(err, data) => {
+//     if (err) {
+//       reject(err);
+//     }
+//     resolve(data);
+//   });
+// });
+
+
+// promise.then(console.log)
+setTimeout(() => 0, 2000)
